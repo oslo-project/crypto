53b4dafe8002d4265bf98767200562ca07f5f6fe
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-06-21 02:05:11 +0900

    export sha224

--- src/sha2/index.ts
+++ src/sha2/index.ts
@@ -1,3 +1,4 @@
+export { SHA224, sha224 } from "./sha224.js";
 export { SHA256, sha256 } from "./sha256.js";
 export { SHA384, sha384 } from "./sha384.js";
 export { SHA512, sha512 } from "./sha512.js";
