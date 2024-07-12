95adbe557281f1a22ec47d43378a875e51be00ca
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-07-12 19:56:08 +0900

    update sha512 test

--- src/sha2/sha512.test.ts
+++ src/sha2/sha512.test.ts
@@ -1,33 +1,14 @@
 import { expect, test } from "vitest";
-import { SHA512 } from "./sha512.js";
-import { concatenateBytes } from "@oslojs/binary";
+import { SHA512, sha512 } from "./sha512.js";
 
-test("SHA512", async () => {
-	const randomValues = crypto.getRandomValues(new Uint8Array(200));
-	for (let i = 0; i < randomValues.byteLength + 1; i++) {
-		const data = randomValues.slice(0, i);
+test("SHA512_256", async () => {
+	const randomValues = crypto.getRandomValues(new Uint8Array(5 * 100));
+	for (let i = 0; i < randomValues.byteLength / 5; i++) {
+		const expected = sha512(randomValues.slice(0, i * 5));
 		const hash = new SHA512();
-		hash.update(data);
-		const result = hash.digest();
-		const expected = new Uint8Array(await crypto.subtle.digest("SHA-512", data));
-		expect(result).toStrictEqual(expected);
-	}
-});
-
-test("SHA512", async () => {
-	const randomValues = crypto.getRandomValues(new Uint8Array(100));
-	for (let i = 0; i < randomValues.byteLength + 1; i++) {
-		const a = randomValues.slice(0, i);
-		for (let j = 0; j < randomValues.byteLength + 1; j++) {
-			const b = randomValues.slice(0, j);
-			const hash = new SHA512();
-			hash.update(a);
-			hash.update(b);
-			const result = hash.digest();
-			const expected = new Uint8Array(
-				await crypto.subtle.digest("SHA-512", concatenateBytes(a, b))
-			);
-			expect(result).toStrictEqual(expected);
+		for (let j = 0; j < i; j++) {
+			hash.update(randomValues.slice(j * 5, (j + 1) * 5));
 		}
+		expect(hash.digest()).toStrictEqual(expected);
 	}
 });
